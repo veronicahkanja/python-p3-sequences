b1cd7698685750ed39a1187059b5a5330f17c383
Veronicah Kanja <kanjaveronicah@gmail.com>
2026-01-15 16:32:18 +0300

    compleete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    if length == 1:
+        print([0])
+        return
+    
+    fib = [0, 1]
+    
+    if length == 2:
+        print(fib)
+        return
+
+    while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])
+
+    print(fib)
